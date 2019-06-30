aec3fac27fb6ee7e6413d3ecd8b7e51d16d7ea6b
Andres Paz <anpaz@microsoft.com>
2019-06-30 02:15:03 -0700

    tweaks

--- build/set-env.sh
+++ build/set-env.sh
@@ -4,7 +4,6 @@ set -e
 : ${BUILD_BUILDNUMBER:="0.0.0.1"}
 : ${BUILD_CONFIGURATION:="Debug"}
 : ${BUILD_VERBOSITY:="m"}
-: ${IQSHARP_HOSTING_ENV:="dev-machine"}
 : ${ASSEMBLY_VERSION:="$BUILD_BUILDNUMBER"}
 : ${NUGET_VERSION:="$ASSEMBLY_VERSION-alpha"}
 : ${NUGET_OUTDIR:="$(cd ..;pwd)/build.nugets"}
