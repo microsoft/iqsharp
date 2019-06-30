01958c863fcbebc817a9bae107a3916f0bbda677
Andres Paz <anpaz@microsoft.com>
2019-06-29 23:23:29 -0700

    Debugging set-env

--- build/set-env.sh
+++ build/set-env.sh
@@ -3,17 +3,17 @@ set -e
 set -x
 
 : ${BUILD_BUILDNUMBER:="0.0.0.1"}
-echo "__vso[task.setvariable variable=BUILD_BUILDNUMBER]$BUILD_BUILDNUMBER"
+echo ##vso[task.setvariable variable=BUILD_BUILDNUMBER]$BUILD_BUILDNUMBER
 : ${BUILD_CONFIGURATION:="Debug"}
-echo "__vso[task.setvariable variable=BUILD_CONFIGURATION]$BUILD_CONFIGURATION"
+echo ##vso[task.setvariable variable=BUILD_CONFIGURATION]$BUILD_CONFIGURATION
 : ${BUILD_VERBOSITY:="m"}
-echo "__vso[task.setvariable variable=BUILD_VERBOSITY]$BUILD_VERBOSITY"
+echo ##vso[task.setvariable variable=BUILD_VERBOSITY]$BUILD_VERBOSITY
 : ${IQSHARP_HOSTING_ENV:="dev-machine"}
-echo "__vso[task.setvariable variable=IQSHARP_HOSTING_ENV]$IQSHARP_HOSTING_ENV"
+echo ##vso[task.setvariable variable=IQSHARP_HOSTING_ENV]$IQSHARP_HOSTING_ENV
 : ${ASSEMBLY_VERSION:="$BUILD_BUILDNUMBER"}
-echo "__vso[task.setvariable variable=ASSEMBLY_VERSION]$ASSEMBLY_VERSION"
+echo ##vso[task.setvariable variable=ASSEMBLY_VERSION]$ASSEMBLY_VERSION
 : ${NUGET_VERSION:="$ASSEMBLY_VERSION-alpha"}
-echo "__vso[task.setvariable variable=NUGET_VERSION]$NUGET_VERSION"
+echo ##vso[task.setvariable variable=NUGET_VERSION]$NUGET_VERSION
 : ${NUGET_OUTDIR:="$(cd ..;pwd)/build.nugets"}
-echo "__vso[task.setvariable variable=NUGET_OUTDIR]$NUGET_OUTDIR"
+echo ##vso[task.setvariable variable=NUGET_OUTDIR]$NUGET_OUTDIR
 
