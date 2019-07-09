ff39d94a5fb503996da3a1af4bd3354854a95d88
Chris Granade <chgranad@microsoft.com>
2019-07-08 23:28:43 -0700

    Update src/Web/WorkspaceController.cs
    Co-Authored-By: bettinaheim <34236215+bettinaheim@users.noreply.github.com>

--- src/Web/WorkspaceController.cs
+++ src/Web/WorkspaceController.cs
@@ -78,7 +78,7 @@ namespace Microsoft.Quantum.IQSharp
         /// <summary>
         /// Simulates the execution of the given operation. 
         /// Supports both, GET and POST.
-        /// If GET, then the parameters are expected as normal query parameters
+        /// If GET, then the parameters are expected as normal query parameters.
         /// If POST, then the parameters are expected as a JSON object in the body.
         /// </summary>
         [HttpGet("{id}/simulate")]
