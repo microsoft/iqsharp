5cf641ad52b0912129539ab684157428aee80a12
Christopher Granade <cgranade@cgranade.com>
2019-08-09 11:49:19 -0700

    Prevent pwsh from capturing NuGet cache.

--- conda-recipes/pwsh/build.sh
+++ conda-recipes/pwsh/build.sh
@@ -1,1 +1,6 @@
+# The user may not have run .NET Core SDK before, so we disable first-time
+# experience to avoid capturing the NuGet cache.
+export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=true
+export NUGET_XMLDOC_MODE=skip
+
 dotnet tool install PowerShell --tool-path $PREFIX/bin --version $PKG_VERSION
