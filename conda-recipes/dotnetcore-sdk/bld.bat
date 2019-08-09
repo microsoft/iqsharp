53a51691b5af56d11404b6d1e5ab88e3d5a946c5
Christopher Granade <cgranade@cgranade.com>
2019-08-09 16:15:40 -0700

    Fix name of activation/deactivation scripts.

--- conda-recipes/dotnetcore-sdk/bld.bat
+++ conda-recipes/dotnetcore-sdk/bld.bat
@@ -34,31 +34,31 @@ set DEACT_D=%PREFIX%\etc\conda\deactivate.d
 md %DEACT_D%
 
 @REM -- cmd activation/deactivation --
-echo set _CONDA_PKG_BACKUP_PATH=%%PATH%% >> %ACT_D%\DOTNET_ROOT.cmd
-echo set _CONDA_PKG_BACKUP_DOTNET_ROOT=%%DOTNET_ROOT%% >> %ACT_D%\DOTNET_ROOT.cmd
-echo set DOTNET_ROOT=%DOTNET_ROOT% >> %ACT_D%\DOTNET_ROOT.cmd
-echo set PATH=%DOTNET_ROOT%;%%PATH%% >> %ACT_D%\DOTNET_ROOT.cmd
-echo set PATH=%%_CONDA_PKG_BACKUP_PATH%% >> %DEACT_D%\DOTNET_ROOT.cmd
-echo set DOTNET_ROOT=%%_CONDA_PKG_BACKUP_DOTNET_ROOT%% >> %DEACT_D%\DOTNET_ROOT.cmd
-echo set _CONDA_PKG_BACKUP_DOTNET_ROOT= >> %DEACT_D%\DOTNET_ROOT.cmd
-echo set _CONDA_PKG_BACKUP_PATH= >> %DEACT_D%\DOTNET_ROOT.cmd
+echo set _CONDA_PKG_BACKUP_PATH=%%PATH%% >> %ACT_D%\dotnet_root.cmd
+echo set _CONDA_PKG_BACKUP_DOTNET_ROOT=%%DOTNET_ROOT%% >> %ACT_D%\dotnet_root.cmd
+echo set DOTNET_ROOT=%DOTNET_ROOT% >> %ACT_D%\dotnet_root.cmd
+echo set PATH=%DOTNET_ROOT%;%%PATH%% >> %ACT_D%\dotnet_root.cmd
+echo set PATH=%%_CONDA_PKG_BACKUP_PATH%% >> %DEACT_D%\dotnet_root.cmd
+echo set DOTNET_ROOT=%%_CONDA_PKG_BACKUP_DOTNET_ROOT%% >> %DEACT_D%\dotnet_root.cmd
+echo set _CONDA_PKG_BACKUP_DOTNET_ROOT= >> %DEACT_D%\dotnet_root.cmd
+echo set _CONDA_PKG_BACKUP_PATH= >> %DEACT_D%\dotnet_root.cmd
 
 @REM -- pwsh activation/deactivation --
-echo $Env:_CONDA_PKG_BACKUP_PATH = "$Env:PATH"; >> %ACT_D%\DOTNET_ROOT.ps1
-echo $Env:_CONDA_PKG_BACKUP_DOTNET_ROOT = "$Env:DOTNET_ROOT"; >> %ACT_D%\DOTNET_ROOT.ps1
-echo $Env:DOTNET_ROOT = "%DOTNET_ROOT%"; >> %ACT_D%\DOTNET_ROOT.ps1
-echo $Env:PATH = "%DOTNET_ROOT%;$Env:PATH"; >> %ACT_D%\DOTNET_ROOT.ps1
-echo $Env:PATH = "$Env:_CONDA_PKG_BACKUP_PATH"; >> %DEACT_D%\DOTNET_ROOT.ps1
-echo $Env:DOTNET_ROOT = "$Env:_CONDA_PKG_BACKUP_DOTNET_ROOT"; >> %DEACT_D%\DOTNET_ROOT.ps1
-echo $Env:_CONDA_PKG_BACKUP_PATH = $null; >> %DEACT_D%\DOTNET_ROOT.ps1
-echo $Env:_CONDA_PKG_BACKUP_DOTNET_ROOT = $null; >> %DEACT_D%\DOTNET_ROOT.ps1
+echo $Env:_CONDA_PKG_BACKUP_PATH = "$Env:PATH"; >> %ACT_D%\dotnet_root.ps1
+echo $Env:_CONDA_PKG_BACKUP_DOTNET_ROOT = "$Env:DOTNET_ROOT"; >> %ACT_D%\dotnet_root.ps1
+echo $Env:DOTNET_ROOT = "%DOTNET_ROOT%"; >> %ACT_D%\dotnet_root.ps1
+echo $Env:PATH = "%DOTNET_ROOT%;$Env:PATH"; >> %ACT_D%\dotnet_root.ps1
+echo $Env:PATH = "$Env:_CONDA_PKG_BACKUP_PATH"; >> %DEACT_D%\dotnet_root.ps1
+echo $Env:DOTNET_ROOT = "$Env:_CONDA_PKG_BACKUP_DOTNET_ROOT"; >> %DEACT_D%\dotnet_root.ps1
+echo $Env:_CONDA_PKG_BACKUP_PATH = $null; >> %DEACT_D%\dotnet_root.ps1
+echo $Env:_CONDA_PKG_BACKUP_DOTNET_ROOT = $null; >> %DEACT_D%\dotnet_root.ps1
 
 @REM -- posix-style activation/deactivation --
-@REM echo export _CONDA_PKG_BACKUP_PATH="$PATH"; >> %ACT_D%\DOTNET_ROOT.sh
-@REM echo export _CONDA_PKG_BACKUP_DOTNET_ROOT="$DOTNET_ROOT"; >> %ACT_D%\DOTNET_ROOT.sh
-@REM echo export DOTNET_ROOT="%DOTNET_ROOT%"; >> %ACT_D%\DOTNET_ROOT.sh
-@REM echo export PATH="$PATH;%DOTNET_ROOT%"; >> %ACT_D%\DOTNET_ROOT.sh
-@REM echo export PATH="$_CONDA_PKG_BACKUP_PATH" >> %DEACT_D%\DOTNET_ROOT.sh
-@REM echo export DOTNET_ROOT="$_CONDA_PKG_BACKUP_DOTNET_ROOT" >> %DEACT_D%\DOTNET_ROOT.sh
-@REM echo unset _CONDA_PKG_BACKUP_DOTNET_ROOT >> %DEACT_D%\DOTNET_ROOT.sh
-@REM echo unset _CONDA_PKG_BACKUP_PATH >> %DEACT_D%\DOTNET_ROOT.sh
+@REM echo export _CONDA_PKG_BACKUP_PATH="$PATH"; >> %ACT_D%\dotnet_root.sh
+@REM echo export _CONDA_PKG_BACKUP_DOTNET_ROOT="$DOTNET_ROOT"; >> %ACT_D%\dotnet_root.sh
+@REM echo export DOTNET_ROOT="%DOTNET_ROOT%"; >> %ACT_D%\dotnet_root.sh
+@REM echo export PATH="$PATH;%DOTNET_ROOT%"; >> %ACT_D%\dotnet_root.sh
+@REM echo export PATH="$_CONDA_PKG_BACKUP_PATH" >> %DEACT_D%\dotnet_root.sh
+@REM echo export DOTNET_ROOT="$_CONDA_PKG_BACKUP_DOTNET_ROOT" >> %DEACT_D%\dotnet_root.sh
+@REM echo unset _CONDA_PKG_BACKUP_DOTNET_ROOT >> %DEACT_D%\dotnet_root.sh
+@REM echo unset _CONDA_PKG_BACKUP_PATH >> %DEACT_D%\dotnet_root.sh
--- conda-recipes/dotnetcore-sdk/build.sh
+++ conda-recipes/dotnetcore-sdk/build.sh
@@ -25,17 +25,17 @@ DEACT_D=$PREFIX/etc/conda/deactivate.d
 mkdir -p $DEACT_D
 
 # -- pwsh activation/deactivation --
-echo "\$Env:_CONDA_PKG_BACKUP_PATH = \"\$Env:PATH\";" >> $ACT_D/DOTNET_ROOT.ps1
-echo "\$Env:_CONDA_PKG_BACKUP_DOTNET_ROOT = \"\$Env:DOTNET_ROOT\";" >> $ACT_D/DOTNET_ROOT.ps1
-echo "\$Env:DOTNET_ROOT = \"$PREFIX/opt/dotnet/\";" >> $ACT_D/DOTNET_ROOT.ps1
-echo "\$Env:PATH = \"$DOTNET_ROOT:\$Env:PATH\";" >> $ACT_D/DOTNET_ROOT.ps1
-echo "\$Env:PATH = \"\$Env:_CONDA_PKG_BACKUP_PATH\";" >> $DEACT_D/DOTNET_ROOT.ps1
-echo "\$Env:DOTNET_ROOT = \"\$Env:_CONDA_PKG_BACKUP_DOTNET_ROOT\";" >> $DEACT_D/DOTNET_ROOT.ps1
+echo "\$Env:_CONDA_PKG_BACKUP_PATH = \"\$Env:PATH\";" >> $ACT_D/dotnet_root.ps1
+echo "\$Env:_CONDA_PKG_BACKUP_DOTNET_ROOT = \"\$Env:DOTNET_ROOT\";" >> $ACT_D/dotnet_root.ps1
+echo "\$Env:DOTNET_ROOT = \"$PREFIX/opt/dotnet/\";" >> $ACT_D/dotnet_root.ps1
+echo "\$Env:PATH = \"$DOTNET_ROOT:\$Env:PATH\";" >> $ACT_D/dotnet_root.ps1
+echo "\$Env:PATH = \"\$Env:_CONDA_PKG_BACKUP_PATH\";" >> $DEACT_D/dotnet_root.ps1
+echo "\$Env:DOTNET_ROOT = \"\$Env:_CONDA_PKG_BACKUP_DOTNET_ROOT\";" >> $DEACT_D/dotnet_root.ps1
 
 # -- posix-style activation/deactivation --
-echo "export _CONDA_PKG_BACKUP_PATH=\"\$PATH\";" >> $ACT_D/DOTNET_ROOT.sh
-echo "export _CONDA_PKG_BACKUP_DOTNET_ROOT=\"$DOTNET_ROOT\";" >> $ACT_D/DOTNET_ROOT.sh
-echo "export DOTNET_ROOT=\"$DOTNET_ROOT\";" >> $ACT_D/DOTNET_ROOT.sh
-echo "export PATH=\"$DOTNET_ROOT:\$PATH\";" >> $ACT_D/DOTNET_ROOT.sh
-echo "export PATH=\"\$_CONDA_PKG_BACKUP_PATH\";" >> $DEACT_D/DOTNET_ROOT.sh
-echo "export DOTNET_ROOT=\"\$_CONDA_PKG_BACKUP_DOTNET_ROOT\";" >> $DEACT_D/DOTNET_ROOT.sh
+echo "export _CONDA_PKG_BACKUP_PATH=\"\$PATH\";" >> $ACT_D/dotnet_root.sh
+echo "export _CONDA_PKG_BACKUP_DOTNET_ROOT=\"$DOTNET_ROOT\";" >> $ACT_D/dotnet_root.sh
+echo "export DOTNET_ROOT=\"$DOTNET_ROOT\";" >> $ACT_D/dotnet_root.sh
+echo "export PATH=\"$DOTNET_ROOT:\$PATH\";" >> $ACT_D/dotnet_root.sh
+echo "export PATH=\"\$_CONDA_PKG_BACKUP_PATH\";" >> $DEACT_D/dotnet_root.sh
+echo "export DOTNET_ROOT=\"\$_CONDA_PKG_BACKUP_DOTNET_ROOT\";" >> $DEACT_D/dotnet_root.sh
