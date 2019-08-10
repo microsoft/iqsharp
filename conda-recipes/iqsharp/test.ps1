ac71c2a50d147a9c2211797ccf874f72ee0fc19a
Christopher Granade <cgranade@cgranade.com>
2019-08-09 17:12:21 -0700

    Fixes to test.

--- conda-recipes/iqsharp/test.ps1
+++ conda-recipes/iqsharp/test.ps1
@@ -4,10 +4,11 @@ $failed = $false;
 $kernels = jupyter kernelspec list --json | ConvertFrom-Json;
 if ($null -eq $kernels.kernelspecs.iqsharp) {
     $failed = $true;
-    Write-Error "IQ# not found in list of kernelspecs, found $($kernels.kernelspecs.PSObject.Properties.Name)."
+    Write-Error "## TEST FAILED: IQ# not found in list of kernelspecs, see kernelspec list below."
+    jupyter kernelspec list
 }
 
 # If any tests failed, raise an error code.
 if ($failed) {
-    return -1;
+    exit -1;
 }
