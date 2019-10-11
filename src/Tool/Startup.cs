22e24a1faad973a309f633a45eef00d8b426e6cc
Bettina Heim <beheim@microsoft.com>
2019-10-11 16:06:59 -0700

    still reference Microsoft.AspNetCore.App depite the warning

--- src/Tool/Startup.cs
+++ src/Tool/Startup.cs
@@ -4,6 +4,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Jupyter.Core;
 using Microsoft.Quantum.IQSharp.Jupyter;
 
 namespace Microsoft.Quantum.IQSharp
