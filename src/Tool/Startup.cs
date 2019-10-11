ce479341a87afe9da424f3993837e2fa05780513
Bettina Heim <beheim@microsoft.com>
2019-10-11 15:22:24 -0700

    reverting use of new feature

--- src/Tool/Startup.cs
+++ src/Tool/Startup.cs
@@ -27,7 +27,7 @@ namespace Microsoft.Quantum.IQSharp
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             app.UseMvc();
         }
