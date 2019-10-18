d852e4bb361bac94421ed41b0cb5aeb469e484ce
Christopher Granade <cgranade@cgranade.com>
2019-10-18 16:21:00 -0700

    --build-only → --no-test

--- build/pack-conda.ps1
+++ build/pack-conda.ps1
@@ -39,7 +39,7 @@ function Pack-CondaRecipe() {
                 Write-Host "##[info]Running: conda build $(Resolve-Path $Path) --python=$_"
                 # See https://stackoverflow.com/a/20950421/267841 for why this works to force conda
                 # to output all log messages to stdout instead of stderr.
-                conda build (Resolve-Path $Path) --build-only --python=$_ 2>&1 | ForEach-Object { "$_" };
+                conda build (Resolve-Path $Path) --no-test --python=$_ 2>&1 | ForEach-Object { "$_" };
             }
     } catch {
         Write-Host "##vso[task.logissue type=warning;]conda build error: $_";
@@ -50,6 +50,7 @@ function Pack-CondaRecipe() {
         $PythonVersions | `
             ForEach-Object {
                 $PackagePath = (conda build (Resolve-Path $Path) --python=$_ --output);
+                Write-Host "##[debug]Expecting to find conda package at $PackagePath.";
                 if (Test-Path $PackagePath) {
                     Copy-Item `
                         $PackagePath `
