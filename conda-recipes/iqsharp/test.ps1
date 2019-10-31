d1b9d17356162e0f40e696807b9f4e8837540f9d
Andres Paz <anpaz@microsoft.com>
2019-10-31 14:38:58 -0700

    Small fixes for release/v0.10 (#52)
    See description.

--- conda-recipes/iqsharp/test.ps1
+++ conda-recipes/iqsharp/test.ps1
@@ -1,12 +1,26 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
 $failed = $false;
+
+$Env:IQSHARP_PACKAGE_SOURCE = "$Env:NUGET_OUTDIR"
 
 # Check that iqsharp is installed as a Jupyter kernel.
 $kernels = jupyter kernelspec list --json | ConvertFrom-Json;
 if ($null -eq $kernels.kernelspecs.iqsharp) {
     $failed = $true;
-    Write-Error "## TEST FAILED: IQ# not found in list of kernelspecs, see kernelspec list below."
+    Write-Error "##vso[task.logissue type=error;]Failed: IQ# not found in list of kernelspecs, see kernelspec list below."
     jupyter kernelspec list
 }
+
+
+Push-Location $PSScriptRoot
+    python test.py
+    if  ($LastExitCode -ne 0) {
+        $failed = $true;
+        Write-Host "##vso[task.logissue type=error;]Failed: IQ# kernel unittests failed"
+    }
+Pop-Location
 
 # If any tests failed, raise an error code.
 if ($failed) {
---
+++ conda-recipes/qsharp/tests.py
@@ -0,0 +1,19 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import qsharp
+
+def test_simple_compile():
+    """
+    Verifies that compile works
+    """
+    op = qsharp.compile( """
+    operation HelloQ() : Result
+    {
+        Message($"Hello from quantum world!"); 
+        return One;
+    }
+""")
+    r = op.simulate()
+    assert r == qsharp.Result.One
+    
