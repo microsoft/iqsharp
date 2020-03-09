2b5c71556176c583f245357521fad7e4e6935c8a
Victor XField <vxfield@outlook.com>
2020-03-08 20:27:37 -0700

    Added comment and better formatting for the MessageExtensions class

--- src/Jupyter/CustomShell/MessageExtensions.cs
+++ src/Jupyter/CustomShell/MessageExtensions.cs
@@ -8,6 +8,9 @@ using System.Linq;
 
 namespace Microsoft.Quantum.IQSharp.Jupyter
 {
+    /// <summary>
+    /// Extensions for the Jupyter Core Message
+    /// </summary>
     public static class MessageExtensions
     {
         /// <summary>
@@ -17,11 +20,11 @@ namespace Microsoft.Quantum.IQSharp.Jupyter
         /// <param name="message">The Jupyter core message</param>
         /// <returns>The message deserialized as the requested type</returns>
         public static T To<T>(this Message message)
-            where T: MessageContent
+            where T : MessageContent
         {
             var content = (message.Content as UnknownContent);
             var result = Activator.CreateInstance<T>();
-            foreach (var property in typeof(T).GetProperties().Where(p=>p.CanWrite))
+            foreach (var property in typeof(T).GetProperties().Where(p => p.CanWrite))
             {
                 var jsonPropertyAttribute = property.GetCustomAttributes(true).OfType<JsonPropertyAttribute>().FirstOrDefault();
                 var propertyName = jsonPropertyAttribute?.PropertyName ?? property.Name;
