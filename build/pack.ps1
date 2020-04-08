6f67295b669b3981d0f0ee9d25c0146b611eafa3
Christopher Granade <cgranade@cgranade.com>
2020-04-07 17:33:37 -0700

    Allow conditionally pulling in additional packages.

--- build/pack.ps1
+++ build/pack.ps1
@@ -143,6 +143,13 @@ if (($Env:ENABLE_DOCKER -eq "false") -or ($Env:ENABLE_PYTHON -eq "false")) {\
     # to build a new image containing all the docs we care about, then `docker cp`
     # them out.
     $tempTag = New-Guid | Select-Object -ExpandProperty Guid;
+    # When building in release mode, we also want to document additional
+    # packages that contribute IQ# magic commands.
+    if ("$Env:BUILD_RELEASETYPE" -eq "release") {
+        $extraPackages = "--package Microsoft.Quantum.Katas --package Microsoft.Quantum.Chemistry.Jupyter";
+    } else {
+        $extraPackages = "";
+    }
     # Note that we want to use a Dockerfile read from stdin so that we can more
     # easily inject the right base image into the FROM line. In doing so,
     # the build context should include the build_docs.py script that we need.
@@ -155,12 +162,11 @@ WORKDIR /workdir
 RUN chown -R `${USER} /workdir
 
 USER `${USER}
-ARG EXTRA_PACKAGES=
 COPY build_docs.py /workdir
 RUN python build_docs.py \
         /workdir/drops/docs/iqsharp-magic \
         microsoft.quantum.iqsharp.magic-ref \
-        `${EXTRA_PACKAGES}    
+        $extraPackages
 "@;
     $dockerfile | docker build -t $tempTag -f - (Join-Path $PSScriptRoot "docs");
     $tempContainer = docker create $tempTag;
