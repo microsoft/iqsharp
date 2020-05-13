9801912f41ba9688b92110d6f91df28657c14418
Ryan Shaffer <rmshaffer@gmail.com>
2020-05-13 13:55:21 -0400

    Fix manifest.ps1 script error

--- build/manifest.ps1
+++ build/manifest.ps1
@@ -1,5 +1,5 @@
-#!/usr/bin/env pwsh
-#Requires -PSEdition Core
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
 
 & "$PSScriptRoot/set-env.ps1"
 
@@ -16,5 +16,5 @@
         ".\src\tool\bin\$Env:BUILD_CONFIGURATION\netcoreapp3.1\Microsoft.Quantum.IQSharp.Jupyter.dll",
         ".\src\tool\bin\$Env:BUILD_CONFIGURATION\netcoreapp3.1\Microsoft.Quantum.IQSharp.Kernel.dll",
         ".\src\tool\bin\$Env:BUILD_CONFIGURATION\netcoreapp3.1\Microsoft.Quantum.IQSharp.Web.dll"
-    ) | ForEach-Object { Get-Item (Join-Path $PSScriptRoot ".." $_) };
+    ) | ForEach-Object { Get-Item (Join-Path $PSScriptRoot (Join-Path ".." $_)) };
 } | Write-Output;
