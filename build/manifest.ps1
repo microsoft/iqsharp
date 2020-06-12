71e8b24385d0eb55065ad8e44119f0fca51073ed
Ryan Shaffer <rmshaffer@gmail.com>
2020-06-12 15:09:23 -0400

    Add self-contained executable outputs to manifest

--- build/manifest.ps1
+++ build/manifest.ps1
@@ -14,6 +14,23 @@
         ".\src\tool\bin\$Env:BUILD_CONFIGURATION\netcoreapp3.1\Microsoft.Quantum.IQSharp.Core.dll",
         ".\src\tool\bin\$Env:BUILD_CONFIGURATION\netcoreapp3.1\Microsoft.Quantum.IQSharp.Jupyter.dll",
         ".\src\tool\bin\$Env:BUILD_CONFIGURATION\netcoreapp3.1\Microsoft.Quantum.IQSharp.Kernel.dll",
-        ".\src\tool\bin\$Env:BUILD_CONFIGURATION\netcoreapp3.1\Microsoft.Quantum.IQSharp.Web.dll"
-    ) | ForEach-Object { Get-Item (Join-Path $PSScriptRoot ".." $_) };
+        ".\src\tool\bin\$Env:BUILD_CONFIGURATION\netcoreapp3.1\Microsoft.Quantum.IQSharp.Web.dll",
+        ".\src\tool\bin\$Env:BUILD_CONFIGURATION\netcoreapp3.1\linux-x64\Microsoft.Quantum.IQSharp.dll",
+        ".\src\tool\bin\$Env:BUILD_CONFIGURATION\netcoreapp3.1\linux-x64\Microsoft.Quantum.IQSharp.Core.dll",
+        ".\src\tool\bin\$Env:BUILD_CONFIGURATION\netcoreapp3.1\linux-x64\Microsoft.Quantum.IQSharp.Jupyter.dll",
+        ".\src\tool\bin\$Env:BUILD_CONFIGURATION\netcoreapp3.1\linux-x64\Microsoft.Quantum.IQSharp.Kernel.dll",
+        ".\src\tool\bin\$Env:BUILD_CONFIGURATION\netcoreapp3.1\linux-x64\Microsoft.Quantum.IQSharp.Web.dll",
+        ".\src\tool\bin\$Env:BUILD_CONFIGURATION\netcoreapp3.1\osx-x64\Microsoft.Quantum.IQSharp.dll",
+        ".\src\tool\bin\$Env:BUILD_CONFIGURATION\netcoreapp3.1\osx-x64\Microsoft.Quantum.IQSharp.Core.dll",
+        ".\src\tool\bin\$Env:BUILD_CONFIGURATION\netcoreapp3.1\osx-x64\Microsoft.Quantum.IQSharp.Jupyter.dll",
+        ".\src\tool\bin\$Env:BUILD_CONFIGURATION\netcoreapp3.1\osx-x64\Microsoft.Quantum.IQSharp.Kernel.dll",
+        ".\src\tool\bin\$Env:BUILD_CONFIGURATION\netcoreapp3.1\osx-x64\Microsoft.Quantum.IQSharp.Web.dll",
+        ".\src\tool\bin\$Env:BUILD_CONFIGURATION\netcoreapp3.1\win10-x64\Microsoft.Quantum.IQSharp.dll",
+        ".\src\tool\bin\$Env:BUILD_CONFIGURATION\netcoreapp3.1\win10-x64\Microsoft.Quantum.IQSharp.Core.dll",
+        ".\src\tool\bin\$Env:BUILD_CONFIGURATION\netcoreapp3.1\win10-x64\Microsoft.Quantum.IQSharp.Jupyter.dll",
+        ".\src\tool\bin\$Env:BUILD_CONFIGURATION\netcoreapp3.1\win10-x64\Microsoft.Quantum.IQSharp.Kernel.dll",
+        ".\src\tool\bin\$Env:BUILD_CONFIGURATION\netcoreapp3.1\win10-x64\Microsoft.Quantum.IQSharp.Web.dll"
+    ) | ForEach-Object { Join-Path $PSScriptRoot ".." $_ }
+      | Where-Object { Test-Path $_ }
+      | ForEach-Object { Get-Item $_ };
 } | Write-Output;
