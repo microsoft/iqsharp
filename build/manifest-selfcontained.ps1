e663a46646304b8362cefdc61b80222f6613c449
Ryan Shaffer <rmshaffer@gmail.com>
2020-06-14 19:57:04 -0700

    Verify manifest files in CI build

--- build/manifest-selfcontained.ps1
+++ build/manifest-selfcontained.ps1
@@ -5,20 +5,20 @@
 
 @{
     Assemblies = @(
-        ".\src\tool\bin\$Env:BUILD_CONFIGURATION\netcoreapp3.1\linux-x64\Microsoft.Quantum.IQSharp.dll",
-        ".\src\tool\bin\$Env:BUILD_CONFIGURATION\netcoreapp3.1\linux-x64\Microsoft.Quantum.IQSharp.Core.dll",
-        ".\src\tool\bin\$Env:BUILD_CONFIGURATION\netcoreapp3.1\linux-x64\Microsoft.Quantum.IQSharp.Jupyter.dll",
-        ".\src\tool\bin\$Env:BUILD_CONFIGURATION\netcoreapp3.1\linux-x64\Microsoft.Quantum.IQSharp.Kernel.dll",
-        ".\src\tool\bin\$Env:BUILD_CONFIGURATION\netcoreapp3.1\linux-x64\Microsoft.Quantum.IQSharp.Web.dll",
-        ".\src\tool\bin\$Env:BUILD_CONFIGURATION\netcoreapp3.1\osx-x64\Microsoft.Quantum.IQSharp.dll",
-        ".\src\tool\bin\$Env:BUILD_CONFIGURATION\netcoreapp3.1\osx-x64\Microsoft.Quantum.IQSharp.Core.dll",
-        ".\src\tool\bin\$Env:BUILD_CONFIGURATION\netcoreapp3.1\osx-x64\Microsoft.Quantum.IQSharp.Jupyter.dll",
-        ".\src\tool\bin\$Env:BUILD_CONFIGURATION\netcoreapp3.1\osx-x64\Microsoft.Quantum.IQSharp.Kernel.dll",
-        ".\src\tool\bin\$Env:BUILD_CONFIGURATION\netcoreapp3.1\osx-x64\Microsoft.Quantum.IQSharp.Web.dll",
-        ".\src\tool\bin\$Env:BUILD_CONFIGURATION\netcoreapp3.1\win10-x64\Microsoft.Quantum.IQSharp.dll",
-        ".\src\tool\bin\$Env:BUILD_CONFIGURATION\netcoreapp3.1\win10-x64\Microsoft.Quantum.IQSharp.Core.dll",
-        ".\src\tool\bin\$Env:BUILD_CONFIGURATION\netcoreapp3.1\win10-x64\Microsoft.Quantum.IQSharp.Jupyter.dll",
-        ".\src\tool\bin\$Env:BUILD_CONFIGURATION\netcoreapp3.1\win10-x64\Microsoft.Quantum.IQSharp.Kernel.dll",
-        ".\src\tool\bin\$Env:BUILD_CONFIGURATION\netcoreapp3.1\win10-x64\Microsoft.Quantum.IQSharp.Web.dll"
-    ) | ForEach-Object { Get-Item (Join-Path $PSScriptRoot ".." $_) };
+        "linux-x64\Microsoft.Quantum.IQSharp.dll",
+        "linux-x64\Microsoft.Quantum.IQSharp.Core.dll",
+        "linux-x64\Microsoft.Quantum.IQSharp.Jupyter.dll",
+        "linux-x64\Microsoft.Quantum.IQSharp.Kernel.dll",
+        "linux-x64\Microsoft.Quantum.IQSharp.Web.dll",
+        "osx-x64\Microsoft.Quantum.IQSharp.dll",
+        "osx-x64\Microsoft.Quantum.IQSharp.Core.dll",
+        "osx-x64\Microsoft.Quantum.IQSharp.Jupyter.dll",
+        "osx-x64\Microsoft.Quantum.IQSharp.Kernel.dll",
+        "osx-x64\Microsoft.Quantum.IQSharp.Web.dll",
+        "win10-x64\Microsoft.Quantum.IQSharp.dll",
+        "win10-x64\Microsoft.Quantum.IQSharp.Core.dll",
+        "win10-x64\Microsoft.Quantum.IQSharp.Jupyter.dll",
+        "win10-x64\Microsoft.Quantum.IQSharp.Kernel.dll",
+        "win10-x64\Microsoft.Quantum.IQSharp.Web.dll"
+    ) | ForEach-Object { Get-Item (Join-Path $Env:SELFCONTAINED_OUTDIR $_) };
 } | Write-Output;
