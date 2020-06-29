5297103026b7f15687ad816358ae813b3a5b041e
Raphael Koh <t-rkoh@microsoft.com>
2020-06-29 18:34:53 -0400

    Reformat comment for XML support

--- src/Tool/Program.cs
+++ src/Tool/Program.cs
@@ -38,7 +38,7 @@ namespace Microsoft.Quantum.IQSharp
 
         /// <summary>
         /// Creates dictionary of kernelspec file names to the embedded resource path
-        /// of all embedded resources found in `Kernel.csproj`.
+        /// of all embedded resources found in <c>Kernel.csproj</c>.
         /// Note: `WithKernelSpecResources` cannot handle keys that include directories
         /// so we treat all path names as period-separated file names.
         /// </summary>
