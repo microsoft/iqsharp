90737594aa4e5992d1186948f03011b0c21de3c4
Raphael Koh <t-rkoh@microsoft.com>
2020-07-02 17:15:04 -0400

    Add WithExecutionPathTracer extension method to SimulatorBase

--- src/Kernel/Extensions.cs
+++ src/Kernel/Extensions.cs
@@ -1,10 +1,10 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Jupyter.Core;
 using Microsoft.Quantum.IQSharp.Jupyter;
+using Microsoft.Quantum.Simulation.Common;
 
 namespace Microsoft.Quantum.IQSharp.Kernel
 {
@@ -23,6 +23,17 @@ namespace Microsoft.Quantum.IQSharp.Kernel
             services.AddSingleton<IMagicSymbolResolver, Kernel.MagicSymbolResolver>();
             services.AddSingleton<IExecutionEngine, Kernel.IQSharpEngine>();
             services.AddSingleton<IConfigurationSource, ConfigurationSource>();
+        }
+
+        /// <summary>
+        ///     Attaches <c>ExecutionPathTracer</c> event listeners to the simulator to generate
+        ///     the <c>ExecutionPath</c> of the operation performed by the simulator.
+        /// </summary>
+        public static T WithExecutionPathTracer<T>(this T sim, ExecutionPathTracer tracer) where T : SimulatorBase
+        {
+            sim.OnOperationStart += tracer.OnOperationStartHandler;
+            sim.OnOperationEnd += tracer.OnOperationEndHandler;
+            return sim;
         }
     }
 }
