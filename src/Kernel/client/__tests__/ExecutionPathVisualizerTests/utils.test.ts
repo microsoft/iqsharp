fd4229f18fd814522b44c2e04495fa1de19214f9
Raphael Koh <t-rkoh@microsoft.com>
2020-07-13 14:19:43 -0400

    Fix test (second try)

--- src/Kernel/client/__tests__/ExecutionPathVisualizerTests/utils.test.ts
+++ src/Kernel/client/__tests__/ExecutionPathVisualizerTests/utils.test.ts
@@ -52,7 +52,7 @@ describe("Testing _getStringWidth", () => {
     });
 
     test("varying size strings", () => {
-        expect(_getStringWidth("H", 14)).toEqual(9);
+        expect(_getStringWidth("I", 14)).toEqual(3);
         expect(_getStringWidth("GateWithASuperLongName", 14)).toEqual(174);
     });
 });
