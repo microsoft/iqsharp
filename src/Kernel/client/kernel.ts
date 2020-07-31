2f8be5668e49aa1bcaac2fe5b4d0ac24a56f3821
Raphael Koh <t-rkoh@microsoft.com>
2020-07-31 11:31:58 -0400

    Add syntax highlighting for %trace command

--- src/Kernel/client/kernel.ts
+++ src/Kernel/client/kernel.ts
@@ -67,7 +67,7 @@ function defineQSharpMode() {
         {
             // built-in magic commands
             token: "builtin",
-            regex: String.raw`(%(config|estimate|lsmagic|package|performance|simulate|toffoli|version|who|workspace))\b`,
+            regex: String.raw`(%(config|estimate|lsmagic|package|performance|simulate|toffoli|version|who|workspace|trace))\b`,
             beginWord: true,
         },
         {
