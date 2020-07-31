83bdf647d1416e2e6e9b59c5ec2461979e39744e
Raphael Koh <t-rkoh@microsoft.com>
2020-07-30 20:11:23 -0400

    Remove dotnet test args

--- build/test.ps1
+++ build/test.ps1
@@ -102,7 +102,7 @@ function Test-JavaScript {
     }
 }
 
-Test-One '../iqsharp.sln' @("AzureClient", "IQSharpEngine", "Workspace")
+Test-One '../iqsharp.sln'
 
 if ($Env:ENABLE_PYTHON -eq "false") {
     Write-Host "##vso[task.logissue type=warning;]Skipping Testing Python packages. Env:ENABLE_PYTHON was set to 'false'."
