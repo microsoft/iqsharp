aa7efe55aa5f772770b9acbbb0c8dc0b9ff89985
Christopher Granade <cgranade@cgranade.com>
2020-10-01 14:00:01 -0700

    [DO NOT MERGE] Forcing test verbosity to diag.

--- build/test.ps1
+++ build/test.ps1
@@ -39,7 +39,7 @@ function Test-One {
 
         dotnet test $project `
             -c $Env:BUILD_CONFIGURATION `
-            -v $Env:BUILD_VERBOSITY `
+            -v diag `
             --no-build `
             --logger trx `
             --filter $_ `
