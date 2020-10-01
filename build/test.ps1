088043c89552124d40a1465e9a6ca6866b106d1e
Christopher Granade <cgranade@cgranade.com>
2020-10-01 11:33:44 -0700

    Fix slight bug in test.ps1.

--- build/test.ps1
+++ build/test.ps1
@@ -113,7 +113,7 @@ function Test-JavaScript {
     }
 }
 
-Test-One '../iqsharp.sln' @("AzureClient", "IQSharpEngine", "Workspace")
+Test-One (Join-Path $PSScriptRoot '../iqsharp.sln') @("AzureClient", "IQSharpEngine", "Workspace")
 
 if ($Env:ENABLE_PYTHON -eq "false") {
     Write-Host "##vso[task.logissue type=warning;]Skipping Testing Python packages. Env:ENABLE_PYTHON was set to 'false'."
