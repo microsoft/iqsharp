d34be65846bed9ce8a6402d8d154ef1991237ffd
Ryan Shaffer <ryan.shaffer@microsoft.com>
2020-10-03 11:18:03 -0400

    Initialize workspace asynchronously (#331)
    Co-authored-by: Chris Granade <chgranad@microsoft.com>

--- src/Tests/Startup.cs
+++ src/Tests/Startup.cs
@@ -40,6 +40,7 @@ namespace Tests.IQSharp
 
             var serviceProvider = services.BuildServiceProvider();
             serviceProvider.GetRequiredService<ITelemetryService>();
+            serviceProvider.GetRequiredService<IWorkspace>().Initialization.Wait();
             return serviceProvider;
         }
 
