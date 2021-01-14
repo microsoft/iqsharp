2e542397d17760ad42831aac67362d8a7fcc0e60
Ricardo Espinoza <ricardoe@microsoft.com>
2021-01-14 00:33:12 -0800

    Update IQSharp Azure client to recent AQ generated files

--- src/AzureClient/Visualization/CloudJobEncoders.cs
+++ src/AzureClient/Visualization/CloudJobEncoders.cs
@@ -17,11 +17,6 @@ namespace Microsoft.Quantum.IQSharp.AzureClient
 {
     internal static class CloudJobExtensions
     {
-        private static DateTime? ToDateTime(this string serializedDateTime) =>
-            DateTime.TryParse(serializedDateTime, CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime dateTime)
-            ? dateTime
-            : null as DateTime?;
-
         internal static Dictionary<string, object?> ToDictionary(this CloudJob cloudJob) =>
             new Dictionary<string, object?>()
             {
@@ -31,9 +26,9 @@ namespace Microsoft.Quantum.IQSharp.AzureClient
                 ["uri"] = cloudJob.Uri.ToString(),
                 ["provider"] = cloudJob.Details.ProviderId,
                 ["target"] = cloudJob.Details.Target,
-                ["creation_time"] = cloudJob.Details.CreationTime.ToDateTime()?.ToUniversalTime(),
-                ["begin_execution_time"] = cloudJob.Details.BeginExecutionTime.ToDateTime()?.ToUniversalTime(),
-                ["end_execution_time"] = cloudJob.Details.EndExecutionTime.ToDateTime()?.ToUniversalTime(),
+                ["creation_time"] = cloudJob.Details.CreationTime?.ToUniversalTime(),
+                ["begin_execution_time"] = cloudJob.Details.BeginExecutionTime?.ToUniversalTime(),
+                ["end_execution_time"] = cloudJob.Details.EndExecutionTime?.ToUniversalTime(),
             };
 
         internal static Table<CloudJob> ToJupyterTable(this IEnumerable<CloudJob> jobsList) =>
@@ -45,9 +40,9 @@ namespace Microsoft.Quantum.IQSharp.AzureClient
                     ("Job ID", cloudJob => $"<a href=\"{cloudJob.Uri}\" target=\"_blank\">{cloudJob.Id}</a>"),
                     ("Job Status", cloudJob => cloudJob.Status),
                     ("Target", cloudJob => cloudJob.Details.Target),
-                    ("Creation Time", cloudJob => cloudJob.Details.CreationTime.ToDateTime()?.ToString() ?? string.Empty),
-                    ("Begin Execution Time", cloudJob => cloudJob.Details.BeginExecutionTime.ToDateTime()?.ToString() ?? string.Empty),
-                    ("End Execution Time", cloudJob => cloudJob.Details.EndExecutionTime.ToDateTime()?.ToString() ?? string.Empty),
+                    ("Creation Time", cloudJob => cloudJob.Details.CreationTime?.ToString() ?? string.Empty),
+                    ("Begin Execution Time", cloudJob => cloudJob.Details.BeginExecutionTime?.ToString() ?? string.Empty),
+                    ("End Execution Time", cloudJob => cloudJob.Details.EndExecutionTime?.ToString() ?? string.Empty),
                 },
                 Rows = jobsList.OrderByDescending(job => job.Details.CreationTime).ToList(),
             };
