f09e92c19989f143abafd0d815e00faa45be8136
Christopher Granade <cgranade@cgranade.com>
2021-02-17 22:20:12 -0800

    Add to noise model encoder.

--- src/Jupyter/Visualization/OpenSystemsEncoders.cs
+++ src/Jupyter/Visualization/OpenSystemsEncoders.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text.Json;
 using Microsoft.Jupyter.Core;
 using Microsoft.Quantum.IQSharp.Jupyter;
+using System.Collections.Generic;
 
 namespace Microsoft.Quantum.Experimental
 {
@@ -17,6 +18,7 @@ namespace Microsoft.Quantum.Experimental
         /// <inheritdoc />
         public string MimeType => MimeTypes.Html;
 
+        /// <inheritdoc />
         public EncodedData? Encode(object displayable)
         {
             if (displayable is MixedState state)
@@ -54,8 +56,23 @@ namespace Microsoft.Quantum.Experimental
         /// <inheritdoc />
         public string MimeType => MimeTypes.Html;
 
+        /// <inheritdoc />
         public EncodedData? Encode(object displayable)
         {
+            string RowForChannel(string name, Channel? channel) =>
+                $@"
+                        <tr>
+                            <th>{name}</th>
+                            <td>
+                                $$
+                                    \left( \begin{{matrix}}
+                                        {channel?.Data?.AsLaTeXMatrixOfComplex() ?? ""}
+                                    \end{{matrix}} \right)
+                                $$
+                            </td>
+                        </tr>
+                ";
+
             if (displayable is NoiseModel noiseModel)
             {
                 return $@"
@@ -65,9 +82,41 @@ namespace Microsoft.Quantum.Experimental
                             <th>Initial state<th>
                             <td>
                                 $$
-                                \left( \begin{{matrix}}
-                                    {(noiseModel.InitialState as MixedState)?.Data?.AsLaTeXMatrixOfComplex() ?? ""}
-                                \end{{matrix}} \right)
+                                    \left( \begin{{matrix}}
+                                        {(noiseModel.InitialState as MixedState)?.Data?.AsLaTeXMatrixOfComplex() ?? ""}
+                                    \end{{matrix}} \right)
+                                $$
+                            </td>
+                        </tr>
+                        {RowForChannel("CNOT", noiseModel?.Cnot)}
+                        {RowForChannel("$I$", noiseModel?.I)}
+                        {RowForChannel("$X$", noiseModel?.X)}
+                        {RowForChannel("$Y$", noiseModel?.Y)}
+                        {RowForChannel("$Z$", noiseModel?.Z)}
+                        {RowForChannel("$H$", noiseModel?.H)}
+                        {RowForChannel("$S$", noiseModel?.S)}
+                        {RowForChannel("$S^{\\dagger}$", noiseModel?.SAdj)}
+                        {RowForChannel("$T$", noiseModel?.T)}
+                        {RowForChannel("$T^{\\dagger}$", noiseModel?.TAdj)}
+                        <tr>
+                            <th>$Z$-measurement effects</th>
+                            <td>
+                                $$
+                                    \left\{{
+                                        {
+                                            string.Join(", ",
+                                                (noiseModel?.ZMeas?.Effects ?? new List<Channel>())
+                                                    .Select(
+                                                        channel => $@"
+                                                            \left( \begin{{matrix}}
+                                                                {channel?.Data?.AsLaTeXMatrixOfComplex() ?? ""}
+                                                            \end{{matrix}} \right)
+                                                        "
+                                                    )
+                                            )
+                                        }
+                                    \right\}}
+                                $$
                             </td>
                         </tr>
                     </table>
