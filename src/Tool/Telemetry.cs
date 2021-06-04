f97f60807058bd70a588b7778727d313b42ca2ab
Christopher Granade <cgranade@cgranade.com>
2021-06-04 12:44:46 -0700

    Revert accidental changes.

--- src/Tool/Telemetry.cs
+++ src/Tool/Telemetry.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-// #if TELEMETRY
+#if TELEMETRY
 
 using System;
 using System.IO;
@@ -249,4 +249,4 @@ namespace Microsoft.Quantum.IQSharp
 
 }
 
-// #endif
+#endif
