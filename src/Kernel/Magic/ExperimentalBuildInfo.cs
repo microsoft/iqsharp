11b527e0a17f67d2db93f6dd82ef0ffbfc8d56ff
Christopher Granade <cgranade@cgranade.com>
2021-06-08 18:34:13 -0700

    Build info as a magic command.

---
+++ src/Kernel/Magic/ExperimentalBuildInfo.cs
@@ -0,0 +1,56 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#nullable enable
+
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Jupyter.Core;
+using Microsoft.Quantum.IQSharp;
+using Microsoft.Quantum.IQSharp.Common;
+using Microsoft.Quantum.IQSharp.Jupyter;
+using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+using System.Text.Json;
+using Microsoft.Extensions.Logging;
+using Microsoft.Quantum.IQSharp.Kernel;
+using System.Collections.Generic;
+
+namespace Microsoft.Quantum.Experimental
+{
+    public class ExperimentalBuildInfoMagic : AbstractMagic
+    {
+
+        /// <summary>
+        ///     Allows for querying noise models and for loading new noise models.
+        /// </summary>
+        public ExperimentalBuildInfoMagic() : base(
+            "experimental.build_info",
+            new Microsoft.Jupyter.Core.Documentation
+            {
+                Summary = "Reports build info for the experimental simulators.",
+                Description = @"
+                    > **⚠ WARNING:** This magic command is **experimental**,
+                    > is not supported, and may be removed from future versions without notice.
+                ".Dedent(),
+                Examples = new string[]
+                {
+                    @"
+                        Return the build info for experimental simulators:
+                        ```
+                        In []: %experimental.build_info
+                        ```
+                    ".Dedent(),
+                }
+            })
+        { }
+
+        /// <inheritdoc />
+        public override ExecutionResult Run(string input, IChannel channel)
+        {
+            // TODO: call OpenSystemsSimulator.BuildInfo once that is enabled.
+            return new Dictionary<string, object>().ToExecutionResult();
+        }
+    }
+}
