decaade8aa6e33db81dae79e483a3f464bdfcb29
Christopher Granade <cgranade@cgranade.com>
2021-07-08 17:42:07 -0700

    Use same technique as build/test.ps1 to run nonrelocatable tests.

--- conda-recipes/qsharp/test.ps1
+++ conda-recipes/qsharp/test.ps1
@@ -4,17 +4,12 @@
 $failed = $false;
 
 $Env:IQSHARP_PACKAGE_SOURCE = "$Env:NUGET_OUTDIR"
+$Env:IQSHARP_LOG_LEVEL = "Debug"
 
 Push-Location (Resolve-Path $PSScriptRoot)
-    pytest --junitxml=junit/integration-test-results.xml tests.py
-    # Also test all the unit tests that came with the Python package itself.
-    # NB: We disable most tests for now, since many of the tests require Q# code that
-    #     isn't copied into the built package.
-    #     Instead, we enable only those tests we know are relocatable.
-    $testPath = Join-Path $Env:SRC_DIR "src/Python/qsharp-core/qsharp/tests/test_iqsharp.py";
-    $cmd = "pytest --junitxml=junit/unit-test-results.xml `"${testPath}::TestCaptureDiagnostics`"";
-    Write-Host "##[debug]Running integration tests with command: $cmd"
-    Invoke-Expression $cmd;
+    $report = "conda-integration-test-results.md";
+    pytest --md $report -v --log-level=Debug tests.py
+    Write-Host "##vso[task.uploadsummary]$(Resolve-Path $report)"
 
     # Check for success.
     if  ($LastExitCode -ne 0) {
@@ -23,6 +18,18 @@ Push-Location (Resolve-Path $PSScriptRoot)
     }
 Pop-Location
 
+# Also test all the unit tests that came with the Python package itself.
+Push-Location "$Env:SRC_DIR/src/Python/qsharp-core/qsharp/tests"
+    $report = "conda-unit-test-results.md";
+    pytest -v --log-level=Debug tests.py
+    Write-Host "##vso[task.uploadsummary]$(Resolve-Path $report)"
+
+    # Check for success.
+    if ($LastExitCode -ne 0) {
+        $failed = $true;
+        Write-Host "##vso[task.logissue type=error;]Failed: Invoking qsharp from python."
+    }
+Pop-Location
 
 # If any tests failed, raise an error code.
 if ($failed) {
