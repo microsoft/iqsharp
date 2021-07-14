73616d8c8a8e18d7cc871eb62fb7f7f45da3610d
Andres Paz <anpaz@microsoft.com>
2021-07-14 12:42:54 -0700

    Enable ConfigurationSource to fallback to environment variables.

--- src/Jupyter/ConfigurationSource/IConfigurationSource.cs
+++ src/Jupyter/ConfigurationSource/IConfigurationSource.cs
@@ -2,11 +2,11 @@
 // Licensed under the MIT License.
 #nullable enable
 
+using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
+
 using Microsoft.Quantum.Experimental;
-using Newtonsoft.Json;
+
 using Newtonsoft.Json.Linq;
 
 namespace Microsoft.Quantum.IQSharp.Jupyter
@@ -33,8 +33,10 @@ namespace Microsoft.Quantum.IQSharp.Jupyter
 
         /// <summary>
         ///     Given the name for a configuration and a default value,
-        ///     returns the given option if it exists, or the default if the
-        ///     user has not specified that option.
+        ///     returns the given option if it exists, the value of an environment variable
+        ///     if an environment variable with the same name or with the name prefixed with 
+        ///     IQSHARP_ exists, or the default if the
+        ///     the option is missing or not available in the environment.
         /// </summary>
         /// <param name="optionName">
         ///     Name of the option to be retrieved.
@@ -42,6 +44,10 @@ namespace Microsoft.Quantum.IQSharp.Jupyter
         /// <param name="defaultValue">
         ///     Value to be returned when the option specified by
         ///     <paramref name="optionName" /> has not been set.
+        /// </param>
+        /// <param name="parser">
+        ///     A parser to parse the string representation of the option
+        ///     (from the environment) into the corresponding expected Type.
         /// </param>
         /// <typeparam name="T">
         ///     The expected type of the given option.
@@ -51,17 +57,40 @@ namespace Microsoft.Quantum.IQSharp.Jupyter
         ///     <paramref name="optionName" /> if it exists, otherwise
         ///     the value of <paramref name="defaultValue" />.
         /// </returns>
-        public T GetOptionOrDefault<T>(string optionName, T defaultValue) =>
-            Configuration.TryGetValue(optionName, out var token)
-            ? token.ToObject<T>() ?? defaultValue
-            : defaultValue;
+        public T GetOptionOrDefault<T>(string optionName, T defaultValue, Func<string, T> parser)
+        {
+            if (Configuration.TryGetValue(optionName, out var token))
+            {
+                return token.ToObject<T>() ?? defaultValue;
+            }
+
+            var key = optionName.ToUpperInvariant().Replace('.', '_');
+            var keyWithPrefix = "IQSHARP_" + key;
+
+            var environment = System.Environment.GetEnvironmentVariable(keyWithPrefix) ?? System.Environment.GetEnvironmentVariable(key);
+            if (environment != null)
+            {
+                return parser(environment);
+            }
+
+            return defaultValue;
+        }
+
+        private bool GetOptionOrDefault(string optionName, bool defaultValue) =>
+            GetOptionOrDefault(optionName, defaultValue, bool.Parse);
+
+        private int GetOptionOrDefault(string optionName, int defaultValue) =>
+            GetOptionOrDefault(optionName, defaultValue, int.Parse);
+
+        private string GetOptionOrDefault(string optionName, string defaultValue) =>
+            GetOptionOrDefault(optionName, defaultValue, e => e);
 
         /// <summary>
         ///     The labeling convention to be used when labeling computational
         ///     basis states (bit string, little-endian or big-endian).
         /// </summary>
         public BasisStateLabelingConvention BasisStateLabelingConvention =>
-            GetOptionOrDefault("dump.basisStateLabelingConvention", BasisStateLabelingConvention.LittleEndian);
+            GetOptionOrDefault("dump.basisStateLabelingConvention", BasisStateLabelingConvention.LittleEndian, Enum.Parse<BasisStateLabelingConvention>);
 
         /// <summary>
         ///     Whether small amplitudes should be truncated when dumping
@@ -79,7 +108,7 @@ namespace Microsoft.Quantum.IQSharp.Jupyter
         ///     is <c>true</c>.
         /// </summary>
         public double TruncationThreshold =>
-            GetOptionOrDefault("dump.truncationThreshold", 1e-10);
+            GetOptionOrDefault("dump.truncationThreshold", 1e-10, double.Parse);
 
         /// <summary>
         ///     Allows for options to view phase as arrows, or in radians
@@ -87,7 +116,7 @@ namespace Microsoft.Quantum.IQSharp.Jupyter
         ///     option to show None. 
         /// </summary>
         public PhaseDisplayStyle PhaseDisplayStyle =>
-            GetOptionOrDefault("dump.phaseDisplayStyle", PhaseDisplayStyle.ArrowOnly);
+            GetOptionOrDefault("dump.phaseDisplayStyle", PhaseDisplayStyle.ArrowOnly, Enum.Parse<PhaseDisplayStyle>);
 
         /// <summary>
         ///     Allows for options to view measurement as horizontal histograms for each
@@ -95,7 +124,7 @@ namespace Microsoft.Quantum.IQSharp.Jupyter
         ///     a decimal, progress bar, both, or None. 
         /// </summary>
         public MeasurementDisplayStyle MeasurementDisplayStyle =>
-            GetOptionOrDefault("dump.measurementDisplayStyle", MeasurementDisplayStyle.BarAndNumber);
+            GetOptionOrDefault("dump.measurementDisplayStyle", MeasurementDisplayStyle.BarAndNumber, Enum.Parse<MeasurementDisplayStyle>);
 
         /// <summary>
         ///     Allows for options to change measurement probability precision when viewing
@@ -129,26 +158,26 @@ namespace Microsoft.Quantum.IQSharp.Jupyter
         ///     using the <c>%trace</c> command.
         /// </summary>
         public TraceVisualizationStyle TraceVisualizationStyle =>
-            GetOptionOrDefault("trace.style", TraceVisualizationStyle.Default);
+            GetOptionOrDefault("trace.style", TraceVisualizationStyle.Default, Enum.Parse<TraceVisualizationStyle>);
 
         /// <summary>
         ///     Specifies the number of qubits that the experimental simulators
         ///     support for use in running Q# programs.
         /// </summary>
         public uint ExperimentalSimulatorCapacity =>
-            GetOptionOrDefault<uint>("experimental.simulators.nQubits", 3);
+            GetOptionOrDefault<uint>("experimental.simulators.nQubits", 3, uint.Parse);
 
         /// <summary>
         ///     Specifies the representation to use for the initial state
         ///     when simulating Q# programs with experimental simulators.
         /// </summary>
         public string ExperimentalSimulatorRepresentation =>
-            GetOptionOrDefault<string>("experimental.simulators.representation", "mixed");
+            GetOptionOrDefault("experimental.simulators.representation", "mixed");
 
         /// <summary>
         ///     Specifies the format used in dumping stabilizer states.
         /// <summary>
         public StabilizerStateVisualizationStyle ExperimentalSimulatorStabilizerStateVisualizationStyle =>
-            GetOptionOrDefault<StabilizerStateVisualizationStyle>("experimental.simulators.stabilizerStateStyle", StabilizerStateVisualizationStyle.MatrixWithDestabilizers);
+            GetOptionOrDefault<StabilizerStateVisualizationStyle>("experimental.simulators.stabilizerStateStyle", StabilizerStateVisualizationStyle.MatrixWithDestabilizers, Enum.Parse<StabilizerStateVisualizationStyle>);
     }
 }
---
+++ src/Tests/ConfigurationSourceTests.cs
@@ -0,0 +1,101 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Quantum.IQSharp;
+using Microsoft.Quantum.IQSharp.Common;
+using Microsoft.Quantum.IQSharp.Jupyter;
+using Microsoft.Quantum.Simulation.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Tests.IQSharp
+{
+    [TestClass]
+    public class ConfigurationSourceTests
+    {
+        [TestMethod]
+        public void Persists()
+        {
+            static void DeleteConfig()
+            {
+                if (File.Exists(ConfigurationSource.ConfigPath))
+                {
+                    File.Delete(ConfigurationSource.ConfigPath);
+                }
+                Assert.IsFalse(File.Exists(ConfigurationSource.ConfigPath));
+            };
+
+            DeleteConfig();
+
+            try
+            {
+                var config = new ConfigurationSource() as IConfigurationSource;
+                Assert.IsNotNull(config);
+                Assert.IsFalse(File.Exists(ConfigurationSource.ConfigPath)); // Make sure the file is not created automatically.
+                Assert.AreEqual(BasisStateLabelingConvention.LittleEndian, config.BasisStateLabelingConvention);
+                Assert.AreEqual(false, config.TruncateSmallAmplitudes);
+                Assert.AreEqual(4, config.MeasurementDisplayPrecision);
+                Assert.AreEqual("mixed", config.ExperimentalSimulatorRepresentation);
+
+                // Change some values
+                config.Configuration["dump.basisStateLabelingConvention"] = JToken.Parse("\"BigEndian\"");
+                config.Configuration["dump.truncateSmallAmplitudes"] = JToken.Parse("true");
+                config.Configuration["dump.measurementDisplayPrecision"] = JToken.Parse("2");
+                config.Configuration["experimental.simulators.representation"] = JToken.Parse("\"simple\"");
+
+                // Save:
+                config.Persist();
+                Assert.IsTrue(File.Exists(ConfigurationSource.ConfigPath));
+
+                config = new ConfigurationSource() as IConfigurationSource;
+                Assert.IsNotNull(config);
+                Assert.AreEqual(BasisStateLabelingConvention.BigEndian, config.BasisStateLabelingConvention);
+                Assert.AreEqual(true, config.TruncateSmallAmplitudes);
+                Assert.AreEqual(2, config.MeasurementDisplayPrecision);
+                Assert.AreEqual("simple", config.ExperimentalSimulatorRepresentation);
+                Assert.AreEqual(MeasurementDisplayStyle.BarAndNumber, config.MeasurementDisplayStyle);
+            }
+            finally
+            {
+                DeleteConfig();
+            }
+        }
+
+        [TestMethod]
+        public void ReadFromEnvironment()
+        {
+            var config = new ConfigurationSource(skipLoading: true) as IConfigurationSource;
+            Assert.IsNotNull(config);
+            Assert.AreEqual(BasisStateLabelingConvention.LittleEndian, config.BasisStateLabelingConvention);
+            Assert.AreEqual(false, config.TruncateSmallAmplitudes);
+            Assert.AreEqual(4, config.MeasurementDisplayPrecision);
+            Assert.AreEqual("mixed", config.ExperimentalSimulatorRepresentation);
+
+            // Set values via environment:
+            System.Environment.SetEnvironmentVariable("DUMP_BASISSTATELABELINGCONVENTION", "BigEndian");
+            System.Environment.SetEnvironmentVariable("IQSHARP_DUMP_TRUNCATESMALLAMPLITUDES", "true");
+            System.Environment.SetEnvironmentVariable("DUMP_MEASUREMENTDISPLAYPRECISION", "2");
+            System.Environment.SetEnvironmentVariable("IQSHARP_EXPERIMENTAL_SIMULATORS_REPRESENTATION", "simple");
+
+            // Read values again, environment should be reflected:
+            Assert.AreEqual(BasisStateLabelingConvention.BigEndian, config.BasisStateLabelingConvention);
+            Assert.AreEqual(true, config.TruncateSmallAmplitudes);
+            Assert.AreEqual(2, config.MeasurementDisplayPrecision);
+            Assert.AreEqual("simple", config.ExperimentalSimulatorRepresentation);
+            Assert.AreEqual(MeasurementDisplayStyle.BarAndNumber, config.MeasurementDisplayStyle);
+
+            // Reset environment:
+            System.Environment.SetEnvironmentVariable("DUMP_BASISSTATELABELINGCONVENTION", null);
+            System.Environment.SetEnvironmentVariable("IQSHARP_DUMP_TRUNCATESMALLAMPLITUDES", null);
+            System.Environment.SetEnvironmentVariable("DUMP_MEASUREMENTDISPLAYPRECISION", null);
+            System.Environment.SetEnvironmentVariable("IQSHARP_EXPERIMENTAL_SIMULATORS_REPRESENTATION", null);
+        }
+    }
+}
