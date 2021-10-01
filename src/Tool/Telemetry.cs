94b3095a09b45f6ffee8a23eef991d64ca8574db
Christopher Granade <cgranade@cgranade.com>
2021-10-01 10:11:54 -0700

    SessionId → KernelInstanceId

--- src/Tool/Telemetry.cs
+++ src/Tool/Telemetry.cs
@@ -235,10 +235,11 @@ namespace Microsoft.Quantum.IQSharp
 
     public static class TelemetryExtensions
     {
-        // Generate a new UUID to use as a session ID.
+        // Generate a new UUID to use in identifying a particular instance
+        // of the kernel process.
         // By making a static property of a non-generic static class, we
         // ensure that there's one session ID per process.
-        internal static readonly Guid SessionId = Guid.NewGuid();
+        internal static readonly Guid KernelInstanceId = Guid.NewGuid();
 
         public static string WithTelemetryNamespace(this string name) =>
             $"Quantum.IQSharp.{name}";
@@ -253,7 +254,7 @@ namespace Microsoft.Quantum.IQSharp
         /// <returns><paramref name="evt"/> for use in fluent chains.</returns>
         public static EventProperties SetCommonProperties(this EventProperties evt)
         {
-            evt.SetProperty("SessionId".WithTelemetryNamespace(), SessionId);
+            evt.SetProperty("KernelInstanceId".WithTelemetryNamespace(), KernelInstanceId);
             evt.SetProperty("ExecutionCount".WithTelemetryNamespace(), TelemetryService.baseEngine?.ExecutionCount?.ToString());
             evt.SetProperty("CurrentTarget".WithTelemetryNamespace(), TelemetryService.client?.ActiveTargetId);
             evt.SetProperty("CurrentSubscription".WithTelemetryNamespace(), (TelemetryService.client as AzureClient.AzureClient)?.ActiveWorkspace?.SubscriptionId, PiiKind.GenericData);
