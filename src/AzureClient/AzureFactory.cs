9b781c00bf8b9a52cb9e0758cb5e3a198361159a
XField <58103249+vxfield@users.noreply.github.com>
2021-10-19 16:34:42 -0700

    Add UserAgent prefix to Azure Quantum APIs

--- src/AzureClient/AzureFactory.cs
+++ src/AzureClient/AzureFactory.cs
@@ -5,6 +5,7 @@
 
 using Azure.Core;
 
+using Azure.Quantum;
 using Microsoft.Azure.Quantum;
 using Microsoft.Quantum.Runtime;
 
@@ -18,13 +19,21 @@ namespace Microsoft.Quantum.IQSharp.AzureClient
             string resourceGroup, 
             string workspaceName, 
             string location, 
-            TokenCredential credential) =>
-                new Azure.Quantum.Workspace(
+            TokenCredential credential)
+        {
+            var options = new QuantumJobClientOptions();
+
+            // This value will be added as a prefix in the UserAgent when
+            // calling the Azure Quantum APIs
+            options.Diagnostics.ApplicationId = "IQ#";
+
+            return new Azure.Quantum.Workspace(
                     subscriptionId: subscriptionId,
                     resourceGroupName: resourceGroup,
                     workspaceName: workspaceName,
                     location: location,
                     credential: credential);
+        }
 
         /// <inheritdoc />
         public IQuantumMachine? CreateMachine(Azure.Quantum.IWorkspace workspace, string targetName, string storageConnectionString) =>
