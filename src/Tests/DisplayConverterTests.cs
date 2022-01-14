6ef04551200a999314068e301288ec034d96d628
Robin Kuzmin <kuzmin.robin@gmail.com>
2022-01-13 18:03:56 -0800

    Switched from Complex[] to IDictionary<int, Complex>.

--- src/Tests/DisplayConverterTests.cs
+++ src/Tests/DisplayConverterTests.cs
@@ -32,10 +32,12 @@ namespace Tests.IQSharp
         {
             QubitIds = new[] {0, 1, 2},
             NQubits = 3,
-            Amplitudes = Enumerable
+            Amplitudes = new Dictionary<int, System.Numerics.Complex>(Enumerable
                 .Range(0, 8)
-                .Select(idx => new System.Numerics.Complex(0, idx))
-                .ToArray()
+                .Select(idx => new KeyValuePair<int, System.Numerics.Complex>(idx, new System.Numerics.Complex(0, idx)))
+                //.Select(idx => new System.Numerics.Complex(0, idx))
+                //.ToArray()
+            )
         };
 
         [TestMethod]
