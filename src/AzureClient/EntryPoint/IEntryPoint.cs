6ef790b52b2e7fbba3ef875867a361b9a1d9b1b5
Cassandra Granade <chgranad@microsoft.com>
2022-02-03 15:48:00 -0800

    Propagate cancellation tokens when refreshing credentials. (#578)
    * Propagate cancellation tokens when refreshing credentials.
    
    * Log and propagate cancellation through entry point generation.
    
    * Slightly more debug logging still.

--- src/AzureClient/EntryPoint/IEntryPoint.cs
+++ src/AzureClient/EntryPoint/IEntryPoint.cs
@@ -6,6 +6,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Quantum.Runtime;
 
@@ -22,7 +23,8 @@ namespace Microsoft.Quantum.IQSharp.AzureClient
         /// </summary>
         /// <param name="machine">The <see cref="IQuantumMachine"/> object representing the job submission target.</param>
         /// <param name="submissionContext">The <see cref="AzureSubmissionContext"/> object representing the submission context for the job.</param>
+        /// <param name="cancellationToken">Cancellation token used to interrupt this submission.</param>
         /// <returns>The details of the submitted job.</returns>
-        public Task<IQuantumMachineJob> SubmitAsync(IQuantumMachine machine, AzureSubmissionContext submissionContext);
+        public Task<IQuantumMachineJob> SubmitAsync(IQuantumMachine machine, AzureSubmissionContext submissionContext, CancellationToken cancellationToken = default);
     }
 }
