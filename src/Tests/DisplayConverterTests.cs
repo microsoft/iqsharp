803de0297a2b957e62d2a8f187b63747931fbd6d
Robin Kuzmin <kuzmin.robin@gmail.com>
2022-02-09 17:39:44 -0800

    Minor clean-up.

--- src/Tests/DisplayConverterTests.cs
+++ src/Tests/DisplayConverterTests.cs
@@ -28,21 +28,12 @@ namespace Tests.IQSharp
     [TestClass]
     public class DisplayConverterTests
     {
-        // public static string Reverse(string s)
-        // {
-        //     char[] charArray = s.ToCharArray();
-        //     System.Array.Reverse( charArray );
-        //     return new string( charArray );
-        // }
-
         private readonly CommonNativeSimulator.DisplayableState testState = new CommonNativeSimulator.DisplayableState
         {
             QubitIds = new[] {0, 1, 2},
             NQubits = 3,
-            // Amplitudes = new Dictionary<string, System.Numerics.Complex>(Enumerable
             Amplitudes = new Dictionary<System.Numerics.BigInteger, System.Numerics.Complex>(Enumerable
                 .Range(0, 8)
-                // .Select(idx => new KeyValuePair<string, System.Numerics.Complex>(Reverse(System.Convert.ToString(idx, 2)), new System.Numerics.Complex(0, idx)))
                 .Select(idx => new KeyValuePair<System.Numerics.BigInteger, System.Numerics.Complex>(
                     new System.Numerics.BigInteger(idx), new System.Numerics.Complex(0, idx)))
             )
