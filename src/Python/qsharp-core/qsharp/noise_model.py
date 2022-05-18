57c4cac227b77e03dfa8b277d423cb1b008d7ddc
Cassandra Granade <cgranade@cgranade.com>
2022-05-17 17:26:24 -0700

    Improve noise model API and display.

--- src/Python/qsharp-core/qsharp/noise_model.py
+++ src/Python/qsharp-core/qsharp/noise_model.py
@@ -9,7 +9,7 @@
 ##
 
 from __future__ import annotations
-from email import generator
+import html
 
 ## DESIGN NOTES ##
 
@@ -37,6 +37,8 @@ __all__ = [
     "set_noise_model",
     "get_noise_model_by_name",
     "set_noise_model_by_name",
+    "to_generator",
+    "NoiseModel",
 
     # State data model
     "Stabilizer",
@@ -52,11 +54,20 @@ __all__ = [
     "UnsupportedGenerator",
     "GeneratorCoset",
 
+    # Instrument data model
+    "EffectsInstrument",
+    "ZMeasInstrument",
+
     # CHP decomposition data model
     "Hadamard",
     "Cnot",
     "Phase",
-    "AdjointPhase"
+    "AdjointPhase",
+
+    # Common utility functions
+    "t1_dissipation",
+    "t2_dissipation",
+    "depolarizing_process",
 ]
 
 ## TYPE DEFINITIONS ##
@@ -67,7 +78,7 @@ if TYPE_CHECKING:
     State = Union[qutip.Qobj, "Stabilizer"]
     Process = Union[qutip.Qobj, "ChpDecompositionProcess", "MixedPauliProcess", "SequenceProcess"]
     Instrument = Any # TODO
-    Generator = Union["ExplicitEigenvalueDecomposition", "UnsupportedGenerator"]
+    Generator = Union[qutip.Qobj, "ExplicitEigenvalueDecomposition", "UnsupportedGenerator"]
 
 ## PUBLIC FUNCTIONS ##
 
@@ -97,19 +108,103 @@ def set_noise_model(noise_model: NoiseModel):
     Sets the current noise model used in simulating Q# programs with the
     `.simulate_noise` method.
     """
-    json_data = dumps(convert_to_rust_style(noise_model._as_qobj()))
+    json_data = dumps(convert_to_rust_style(_as_jobj(noise_model)))
     qsharp.client.set_noise_model(json_data)
 
-def set_noise_model_by_name(name):
+def set_noise_model_by_name(name, **kwargs):
     """
     Sets the current noise model used in simulating Q# programs with the
     `.simulate_noise` method to a built-in noise model, given by name.
 
-    :param name: The name of the noise model to be returned (either `ideal`
+    Any additional keyword arguments are used to modify properties of the
+        noise model before it is set.
+
+    :param name: The name of the noise model to be set (either `ideal`
         or `ideal_stabilizer`).
     """
     qsharp.client.set_noise_model_by_name(name)
+    if kwargs:
+        model = get_noise_model()
+        for name, value in kwargs.items():
+            setattr(model, name, value)
+        set_noise_model(model)
 
+def to_generator(hamiltonian, *dissipators, pre=None, post=None) -> GeneratorCoset:
+    """
+    Given a Hamiltonian and possibly some dissipative terms, returns a
+    generator representing evolution of density operators under that
+    Hamiltonian.
+
+    Note that only generators represented by normal matrices (that is, matrices
+    that commute with their adjoints) are currently supported; a warning will
+    result if a dissipator would result in non-normal evolution.
+
+    For example, continuous-time generalized amplitude damping dissipators can result in
+    non-normal generators in some cases.
+
+    :param hamiltonian: The coherent portion of evolution to be represented.
+    :param dissipators: Lindblad (aka "jump") operators representing
+        continuous-time noise.
+    :param pre: A quantum process applied before continuous-time noise.
+    :param post: A quantum process applied following continuous-time noise.
+    """
+    import qutip as qt
+
+    ident = qt.qeye(hamiltonian.dims[0][0])
+    liouvillian = qt.tensor(ident, hamiltonian) - qt.tensor(hamiltonian.trans(), ident)
+
+    generator = -1j * liouvillian
+
+    for element in dissipators:
+        if not isinstance(element, list):
+            element = [element]
+
+        for diss in element:
+            li_dag_li = diss.dag() * diss
+            generator += qt.tensor(diss.conj(), diss) - 0.5 * (
+                qt.tensor(ident, li_dag_li) +
+                qt.tensor(li_dag_li.trans(), ident)
+            )
+
+    normal_check = generator.dag() * generator - generator * generator.dag()
+    if normal_check.norm() >= 1e-8:
+        import warnings
+        warnings.warn(f"Generator was not normal ({normal_check.norm()}), noise models including this generator may not be supported for use with simulate_noise method.")
+
+    return GeneratorCoset(generator=generator, pre=pre, post=post)
+
+def t2_dissipation(t2: float):
+    """
+    Represents dissipation from dephasing (a.k.a. T2) evolution.
+
+    :param t2: The characteristic time T2 for the given dephasing dissipator.
+    """
+    import qutip as qt
+    import numpy as np
+    return [np.sqrt(1 / t2) * qt.sigmaz()]
+
+def t1_dissipation(t1: float):
+    """
+    Represents dissipation from generalized amplitude damping (a.k.a. T1)
+    evolution, with the maximally mixed state as its fixed point.
+
+    :param t2: The characteristic time T1 for the given GAD dissipator.
+    """
+    import qutip as qt
+    import numpy as np
+    p_fixed = 0.5
+    return [
+        np.sqrt(p_fixed) * np.sqrt(1 / t1) * qt.sigmap(),
+        np.sqrt(1 - p_fixed) * np.sqrt(1 / t1) * qt.sigmam(),
+    ]
+
+def depolarizing_process(p):
+    import qutip as qt
+    i = qt.to_super(qt.qeye(2))
+    x = qt.to_super(qt.sigmax())
+    y = qt.to_super(qt.sigmay())
+    z = qt.to_super(qt.sigmaz())
+    return p * i + (1 - p) / 4 * (i + x + y + z)
 
 ## PUBLIC DATA MODEL ##
 
@@ -131,7 +226,14 @@ class NoiseModel:
     ry: GeneratorCoset
     rz: GeneratorCoset
 
-    def _as_qobj(self):
+    def _as_jobj(self):
+        def ensure_coset(o):
+            return (
+                o
+                if isinstance(o, GeneratorCoset)
+                else GeneratorCoset(generator=o, pre=None, post=None)
+            )
+
         return {
             'initial_state': self.initial_state,
             'cnot': self.cnot,
@@ -145,10 +247,85 @@ class NoiseModel:
             'y': self.y,
             'z': self.z,
             'z_meas': self.z_meas,
-            'rx': self.rx,
-            'ry': self.ry,
-            'rz': self.rz,
+            'rx': ensure_coset(self.rx)._as_jobj(),
+            'ry': ensure_coset(self.ry)._as_jobj(),
+            'rz': ensure_coset(self.rz)._as_jobj(),
         }
+
+    def _repr_html_(self):
+        def try_repr(value):
+            return (
+                "Unsupported" if isinstance(value, (UnsupportedGenerator, UnsupportedProcess)) else
+                value._repr_html_() if hasattr(value, "_repr_html_") else
+                value._repr_latex_() if hasattr(value, "_repr_latex_") else
+                f"<tt>{html.escape(repr(value))}</tt>"
+            )
+        def gate_row(name):
+            gate = getattr(self, name)
+            text = try_repr(gate)
+            return f"""
+                <tr>
+                    <td>{name}</td>
+                    <td style="text-align: left" colspan="2">{text}</td>
+                </tr>
+            """
+        def cont_gate_row(name):
+            gate = getattr(self, name)
+            rowspan = 1 + (1 if gate.pre is not None else 0) + (1 if gate.post is not None else 0)
+            res = f"""
+                <tr>
+                    <td rowspan="{rowspan}">{name}</td>
+                    <td style="text-align: left">Generator</td>
+                    <td style="text-align: left">{try_repr(gate.generator)}</td>
+                </tr>
+            """
+            if gate.pre is not None:
+                res += f"""
+                    <tr>
+                        <td style="text-align: left">Pre</td>
+                        <td style="text-align: left">{try_repr(gate.pre)}</td>
+                    </tr>
+                """
+            if gate.post is not None:
+                res += f"""
+                    <tr>
+                        <td style="text-align: left">Post</td>
+                        <td style="text-align: left">{try_repr(gate.post)}</td>
+                    </tr>
+                """
+            return res
+
+        gates = "\n".join(
+            gate_row(name)
+            for name in (
+                'i', 's', 's_adj', 't', 't_adj', 'h', 'x', 'y', 'z',
+            )
+        )
+        cont_gates = "\n".join(
+            cont_gate_row(name)
+            for name in (
+                'rx', 'ry', 'rz',
+            )
+        )
+        return f"""
+            <table>
+                <caption>Noise model</caption>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th style="text-align: left" colspan="2">Value</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Initial state</td>
+                        <td style="text-align: left" colspan="2">{try_repr(self.initial_state)}</td>
+                    </tr>
+                    {gates}
+                    {cont_gates}
+                </tbody>
+            </table>
+        """
 
 @dataclasses.dataclass
 class Stabilizer:
@@ -185,6 +362,32 @@ class ExplicitEigenvalueDecomposition():
     vectors: Any
     n_qubits: int
 
+    @classmethod
+    def _from_qobj(cls, qobj):
+        import qutip as qt
+        import numpy as np
+
+        vals, vecs = qobj.eigenstates()
+        # TODO: Double check n_qubits!
+        # TODO: Truncate zero eigenvalues.
+        return cls(
+            vals,
+            np.array([np.asarray(vec.data.todense())[:, 0] for vec in vecs]),
+            1
+        )
+
+    def _repr_latex_(self):
+        import qutip as qt
+        res = qt.qzero([[2] * self.n_qubits] * 2)
+        for val, vec in zip(self.values, self.vectors):
+            proj = val * qt.Qobj(vec) * qt.Qobj(vec).dag()
+            proj.dims = res.dims
+            res += proj
+        return res._repr_latex_().replace(
+            "Quantum object",
+            "Quantum object w/ precomputed eigendecomposition"
+        )
+
     def _as_jobj(self):
         return {
             'n_qubits': self.n_qubits,
@@ -208,17 +411,31 @@ class UnsupportedGenerator():
 
 @dataclasses.dataclass
 class GeneratorCoset():
-    generator: Generator
+    generator: Union[Generator, "qutip.Qobj"]
     pre: Optional[Any]
     post: Optional[Any]
 
     def _as_jobj(self):
+        try:
+            import qutip as qt
+        except:
+            qt = None
+
         obj = {}
+
         if self.pre is not None:
             obj['pre'] = self.pre
+
         if self.post is not None:
             obj['post'] = self.post
-        obj['generator'] = self.generator._as_jobj()
+
+        generator = self.generator
+
+        if qt is not None and isinstance(generator, qt.Qobj):
+            generator = ExplicitEigenvalueDecomposition._from_qobj(generator)
+
+        obj['generator'] = _as_jobj(generator)
+
         return obj
 
 @dataclasses.dataclass
@@ -238,6 +455,9 @@ class SequenceProcess():
 class Hadamard():
     idx_target: int
 
+    def __repr__(self):
+        return f"Hadamard({self.idx_target})"
+
     def _as_jobj(self):
         return {
             'Hadamard': self.idx_target
@@ -246,6 +466,9 @@ class Hadamard():
 @dataclasses.dataclass
 class Phase():
     idx_target: int
+
+    def __repr__(self):
+        return f"Phase({self.idx_target})"
 
     def _as_jobj(self):
         return {
@@ -256,6 +479,9 @@ class Phase():
 class AdjointPhase():
     idx_target: int
 
+    def __repr__(self):
+        return f"AdjointPhase({self.idx_target})"
+
     def _as_jobj(self):
         return {
             'AdjointPhase': self.idx_target
@@ -265,6 +491,9 @@ class AdjointPhase():
 class Cnot():
     idx_control: int
     idx_target: int
+
+    def __repr__(self):
+        return f"Cnot({self.idx_control}, {self.idx_target})"
 
     def _as_jobj(self):
         return {
@@ -316,6 +545,26 @@ class MixedPauliProcess():
                     ]
                     for (pr, ops) in self.operators
                 ]
+            }
+        }
+
+@dataclasses.dataclass
+class EffectsInstrument():
+    effects: List[Process]
+
+    def _as_jobj(self):
+        return {
+            'Effects': list(map(qobj_to_rust_style, self.effects))
+        }
+
+@dataclasses.dataclass
+class ZMeasInstrument():
+    pr_readout_error: float
+
+    def _as_jobj(self):
+        return {
+            'ZMeasurement': {
+                'pr_readout_error': self.pr_readout_error
             }
         }
 
@@ -434,6 +683,16 @@ def convert_to_qobjs(json_obj):
 
         ChpDecompositionProcess._from_jobj(json_obj)
         if 'data' in json_obj and 'ChpDecomposition' in json_obj['data'] else
+
+        SequenceProcess(json_obj['n_qubits'], list(map(convert_to_qobjs, json_obj['data']['Sequence'])))
+        if 'data' in json_obj and 'Sequence' in json_obj['data'] else
+
+        # TODO: Hook up effects instrument, z instrument.
+        EffectsInstrument(list(map(convert_to_qobjs, json_obj['Effects'])))
+        if 'Effects' in json_obj else
+
+        ZMeasInstrument(json_obj['ZMeasurement']['pr_readout_error'])
+        if 'ZMeasurement' in json_obj else
 
         # Since we deserialized unsupported generators above, we know that
         # this unsupported must be a process.
