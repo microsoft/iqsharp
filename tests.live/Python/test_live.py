9d91f1df6529fa8bac06508346adc8f1d65cfd4c
Warren Jones <v-wjones@microsoft.com>
2022-08-18 21:05:45 -0700

    Update Quantinuum target names in live test

--- tests.live/Python/test_live.py
+++ tests.live/Python/test_live.py
@@ -171,6 +171,8 @@ def test_quantinuum_targets():
     target_ids = [t.id for t in targets]
     assert 'quantinuum.hqs-lt-s1' in target_ids
     assert 'quantinuum.hqs-lt-s1-apival' in target_ids
+    assert 'quantinuum.qpu.h1-1' in target_ids
+    assert 'quantinuum.sim.h1-1sc' in target_ids
 
 def test_quantinuum_submit():
     """
@@ -190,6 +192,10 @@ def test_quantinuum_submit():
     t = qsharp.azure.target("quantinuum.hqs-lt-s1-apival")
     assert isinstance(t, qsharp.azure.AzureTarget)
     assert t.id == "quantinuum.hqs-lt-s1-apival"
+
+    t = qsharp.azure.target("quantinuum.sim.h1-1sc")
+    assert isinstance(t, qsharp.azure.AzureTarget)
+    assert t.id == "quantinuum.sim.h1-1sc"
 
     job = qsharp.azure.submit(RunTeleport, doPlus=expected)
     assert isinstance(job, qsharp.azure.AzureJob)
