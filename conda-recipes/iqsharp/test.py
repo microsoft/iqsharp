8c37ecb1c12c9ffb0eeec3fc6b0fc0cb98cfaf3d
Cassandra Granade <cgranade@cgranade.com>
2022-09-28 20:59:11 -0700

    Add test for metadata key in jupyter protocol.

--- conda-recipes/iqsharp/test.py
+++ conda-recipes/iqsharp/test.py
@@ -13,5 +13,20 @@ class MyKernelTests(jupyter_kernel_test.KernelTests):
     # language_info.name in a kernel_info_reply should match this
     language_name = "qsharp"
 
+    def test_iqsharp_metadata_is_correct(self):
+        """
+        Some clients, e.g. nteract, require that metadata on displayable data
+        is convertable to dict[str, dict[str, Any]]; we test that this is the
+        case here.
+        """
+        self.flush_channels()
+        reply, output_msgs = self.execute_helper("%version")
+        self.assertEqual(output_msgs[0]['header']['msg_type'], 'display_data')
+        self.assert_(isinstance(output_msgs[0]['content']['metadata'], dict))
+        for mime_type, contents in output_msgs[0]['content']['metadata'].items():
+            self.assert_(isinstance(mime_type, str))
+            self.assert_(isinstance(contents, dict))
+            self.assertEqual(contents, {})
+
 if __name__ == '__main__':
     unittest.main()
